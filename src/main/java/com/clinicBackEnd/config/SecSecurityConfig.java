e45e58e3a5ddde86b3368e2ef451cbb88de876d7
PauliusKraujelis <PauliusKraujelis@gmail.com>
2023-08-09 15:42:24 +0300

    basic security

---
+++ src/main/java/com/clinicBackEnd/config/SecSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.clinicBackEnd.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+	@Bean
+    InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+            .password(passwordEncoder().encode("user1Pass"))
+            .roles("USER")
+            .build();
+        UserDetails user2 = User.withUsername("user2")
+            .password(passwordEncoder().encode("user2Pass"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        	.authorizeHttpRequests(authorize -> authorize
+        				.requestMatchers("/admin/a").hasRole("ADMIN")
+        				.requestMatchers("/anonymous*").anonymous()
+        				.requestMatchers("/login*").permitAll()
+        				.anyRequest().authenticated()
+        	)
+        	
+        	.formLogin(withDefaults()
+        	)
+        
+        	.logout(logout -> logout
+        			.logoutUrl("/anonymous")
+        			.deleteCookies("JSESSIONID")
+        	)
+        			
+        ;
+	    return http.build();
+	}
+    
+
+	@Bean 
+	PasswordEncoder passwordEncoder() { 
+	    return new BCryptPasswordEncoder(); 
+	}
+}
